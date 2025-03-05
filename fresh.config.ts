99b6324d9ee3721a0502e09e966d40f1601f07f8
heathweaver <heath.weaver@remote-executive.com>
2025-03-05 10:53:07 +0100

    some main config files

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
+import { authPlugin } from "./plugins/auth/mod.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [
+    tailwind(),
+    authPlugin({
+      routes: {
+        login: "/auth/login",
+        signup: "/auth/signup",
+        callback: "/auth/callback",
+        logout: "/auth/logout"
+      }
+    })
+  ]
 });
